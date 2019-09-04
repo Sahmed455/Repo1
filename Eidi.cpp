b0fa9c3823efec925a8c66d2b731aef4a34e9662
Sahmed455 <ahmedsarfaraz455@gmail.com>
2019-09-05 02:15:30 +0530

    Eidi.cpp

---
+++ Eidi.cpp
@@ -0,0 +1,24 @@
+#include<stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int i=0;t,a[3],c[18],j=0,k=0;
+	cin>>t;
+	while(i<t)
+	{
+		while(j<3)
+		{
+			cin>>a[j];
+			j++;
+		}
+		cout<<endl;
+		while(k<0)
+		{
+			cin>>c[a[k]];
+			k++;
+		}
+		i++;
+	}
+	if(c[a[0]]<c[a]
